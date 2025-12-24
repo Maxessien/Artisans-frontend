0c930843b93aebb4e0bf619c9f70c93f5cc1e747
Maxessien <essienmax484@gmail.com>
2025-12-24 16:35:30 +0100

    Added paystack and flutterwave svg icon

--- src/components/svg_components/PaymentSvg.jsx
+++ src/components/svg_components/PaymentSvg.jsx
@@ -13,7 +13,7 @@ const PaymentCompleteIcon = (props) => {
       <circle cx="89.9995" cy="89.9995" r="67.6802" fill="#41D52B" />
       <mask
         id="mask0_2225_862"
-        style="mask-type:luminance"
+        style={{ maskType: "luminance" }}
         maskUnits="userSpaceOnUse"
         x="46"
         y="44"
@@ -46,4 +46,94 @@ const PaymentCompleteIcon = (props) => {
   );
 };
 
-export { PaymentCompleteIcon };
+const FlutterWaveIcon = (props) => {
+  return (
+    <svg
+      {...props}
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      viewBox="0 0 176 144.7"
+      style={{ enableBackground: "new 0 0 176 144.7" }}
+      xmlSpace="preserve"
+    >
+      <style type="text/css">
+        {`.st0{fill:#009A46;}.st1{fill:#FF5805;}.st2{fill:#F5AFCB;}.st3{fill:#FF9B00;}`}
+      </style>
+      <metadata>
+        <sfw xmlns="ns_sfw;">
+          <slices></slices>
+          <sliceSourceBounds
+            bottomLeftOrigin="true"
+            height="144.7"
+            width="176"
+            x="-258.1"
+            y="-272.6"
+          ></sliceSourceBounds>
+        </sfw>
+      </metadata>
+      <g>
+        <path
+          className="st0"
+          d="M0,31.6c0-9.4,2.7-17.4,8.5-23.1l10,10C7.4,29.6,17.1,64.1,48.8,95.8s66.2,41.4,77.3,30.3l10,10
+    		c-18.8,18.8-61.5,5.4-97.3-30.3C14,80.9,0,52.8,0,31.6z"
+        ></path>
+        <path
+          className="st1"
+          d="M63.1,144.7c-9.4,0-17.4-2.7-23.1-8.5l10-10c11.1,11.1,45.6,1.4,77.3-30.3s41.4-66.2,30.3-77.3l10-10
+    		c18.8,18.8,5.4,61.5-30.3,97.3C112.4,130.7,84.2,144.7,63.1,144.7z"
+        ></path>
+        <path
+          className="st2"
+          d="M140.5,91.6c-6.1-17.5-18.5-36.2-34.9-52.6C69.8,3.2,27.1-10.1,8.3,8.6l0,0C7,10,8.2,13.3,10.9,16
+    		s6.1,3.9,7.4,2.6c11.1-11.1,45.6-1.4,77.3,30.3c15,15,26.2,31.8,31.6,47.3c4.7,13.6,4.3,24.6-1.2,30.1l0,0
+    		c-1.3,1.3-0.2,4.6,2.6,7.4s6.1,3.9,7.4,2.6C145.6,126.6,147.2,110.7,140.5,91.6z"
+        ></path>
+        <path
+          className="st3"
+          d="M167.5,8.6C157.9-1,142-2.6,122.9,4c-17.5,6.1-36.2,18.5-52.6,34.9c-35.8,35.8-49.1,78.5-30.3,97.3l0,0
+    		c1.3,1.3,4.7,0.2,7.4-2.6s3.9-6.1,2.6-7.4c-11.1-11.1-1.4-45.6,30.3-77.3c15-15,31.8-26.2,47.2-31.6c13.6-4.7,24.6-4.3,30.1,1.2
+    		l0,0c1.3,1.3,4.6,0.2,7.4-2.6S168.9,10,167.5,8.6z"
+        ></path>
+      </g>
+    </svg>
+  );
+};
+
+const PayStackIcon = (props) => {
+  return (
+    <svg
+      {...props}
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      viewBox="0 0 44.6 44.3"
+      style={{ enableBackground: "new 0 0 44.6 44.3" }}
+      xmlSpace="preserve"
+    >
+      <style type="text/css">{`.st0{fill:#00C3F7;}`}</style>
+      <g>
+        <g>
+          <path
+            className="st0"
+            d="M39.9,0H2.3C1.1,0,0,1.1,0,2.4v4.2C0,7.9,1.1,9,2.3,9h37.6c1.3,0,2.3-1.1,2.4-2.4V2.4C42.3,1.1,41.2,0,39.9,0
+			L39.9,0z M39.9,23.6H2.3c-0.6,0-1.2,0.3-1.7,0.7C0.2,24.7,0,25.3,0,26v4.2c0,1.3,1.1,2.4,2.3,2.4h37.6c1.3,0,2.3-1,2.4-2.4V26
+			C42.3,24.6,41.2,23.6,39.9,23.6L39.9,23.6z M23.5,35.4H2.3c-0.6,0-1.2,0.2-1.6,0.7c-0.4,0.4-0.7,1-0.7,1.7V42
+			c0,1.3,1.1,2.4,2.3,2.4h21.1c1.3,0,2.3-1.1,2.3-2.4v-4.3C25.8,36.4,24.8,35.4,23.5,35.4L23.5,35.4z M42.3,11.8h-40
+			c-0.6,0-1.2,0.2-1.6,0.7c-0.4,0.4-0.7,1-0.7,1.7v4.2c0,1.3,1.1,2.4,2.3,2.4h39.9c1.3,0,2.3-1.1,2.3-2.4v-4.2
+			C44.6,12.9,43.6,11.8,42.3,11.8L42.3,11.8z M42.3,11.8"
+          ></path>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export { FlutterWaveIcon, PaymentCompleteIcon, PayStackIcon };
+

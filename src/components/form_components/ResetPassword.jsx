e12eb558448e50e63eda1ce6b209105b1032f8e7
Maxessien <essienmax484@gmail.com>
2026-01-09 19:27:11 +0100

    Fixed reset password imports and send sign in link params

--- src/components/form_components/ResetPassword.jsx
+++ src/components/form_components/ResetPassword.jsx
@@ -1,18 +1,20 @@
 "use client"
 
 import { toast } from "react-toastify";
-import logger from "../../src/utils/logger";
-import AuthFormField from "./../../src/components/form_components/AuthFormField";
+import logger from "../../utils/logger";
+import AuthFormField from "./AuthFormField";
 import { useMutation } from "@tanstack/react-query";
 import { sendSignInLinkToEmail } from "firebase/auth";
-import { auth } from "../../../backend/configs/fbConfigs";
-import ButtonLink from "../../src/components/reusable_components/ButtonLink";
+import { auth } from "../../../firebase/fb_config.js";
+import ButtonLink from "../reusable_components/ButtonLink";
 
 const ForgotPassword = () => {
   const resetPassword = async ({ email }) => {
+	logger.log("Email info", email)
     try {
       await sendSignInLinkToEmail(auth, email, {
         url: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/signin-link?email=${email}`,
+	handleCodeInApp: true,
       });
       return { success: true };
     } catch (err) {
@@ -29,19 +31,21 @@ const ForgotPassword = () => {
   if (isSuccess) {
     return (
       <>
+	<div className="space-y-3 px-6 pt-7">
         <h1 className="text-2xl text-(--text-primary) text-center font-normal">Email verification link sent</h1>
         <p className="text-base text-(--text-primary) text-center font-normal">Check inbox for password reset link</p>
-        <ButtonLink rounded="6px" width="100%" href="/reset-password">
+        <ButtonLink rounded="md" width="full" href="/reset-password">
           Change Email
         </ButtonLink>
+	</div>
       </>
     );
   }
 
   return (
     <>
-      <div className="form_page_section">
-        <h1>Forgot Password</h1>
+      <div className="px-6 space-y-3 pt-10">
+        <h1 className="text-2xl text-(--text-primary) text-center font-normal">Forgot Password</h1>
         <AuthFormField
           submitFunction={mutateAsync}
           isSubmitting={isPending}
